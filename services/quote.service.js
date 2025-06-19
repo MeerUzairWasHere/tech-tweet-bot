4d4188e9ac30b02fc69e700e7e885fd7700b4977
Meer Uzair <meer.uxair007@gmail.com>
2025-06-19 10:48:25 +0530

    changed url

--- services/quote.service.js
+++ services/quote.service.js
@@ -2,9 +2,7 @@ const axios = require("axios");
 
 async function getTechQuote() {
   try {
-    // Using Techy API
     const response = await axios.get("https://techy-api.vercel.app/api/json");
-    console.log(response.data.message);
     return response.data.message;
   } catch (error) {
     console.error("Error fetching quote:", error);
