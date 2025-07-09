d0a0bc7db25343f38f2fb2fd4424545d0a742ecf
Meer Uzair <meer.uxair007@gmail.com>
2025-07-09 21:01:09 +0530

    Add health check endpoint and cron job
    Introduced a new /api/v1/healthz endpoint for server health checks. Added a cron job using node-cron and axios to periodically ping the health check endpoint every 14 minutes.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 require("dotenv").config();
+const cron = require("node-cron");
+const axios = require("axios");
 const express = require("express");
 const { postTweet } = require("./services/twitter.service");
 const { getTechQuote } = require("./services/quote.service");
@@ -7,7 +9,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
 
 app.get("/api/tweet-now", async (req, res) => {
   try {
@@ -19,6 +20,21 @@ app.get("/api/tweet-now", async (req, res) => {
   }
 });
 
+app.get("/api/v1/healthz", (req, res) => {
+  res.json({ success: true, msg: "Server is running" });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+cron.schedule("*/14 * * * *", async () => {
+  try {
+    const response = await axios.get(
+      `https://tech-tweet-bot.onrender.com/api/v1/healthz`
+    );
+    console.log(`Health check successful: ${response.data.msg}`);
+  } catch (error) {
+    console.error(`Health check failed: ${error.message}`);
+  }
 });
