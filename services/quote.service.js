a0096c72fabaef534333b77ea04aa0dbd7f4d65b
Meer Uzair <meer.uxair007@gmail.com>
2025-07-23 20:19:48 +0530

    randomizer

--- services/quote.service.js
+++ services/quote.service.js
@@ -5,10 +5,60 @@ const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
 async function getTechQuote() {
   try {
+    const topics = [
+      "MongoDB",
+      "SQL",
+      "PostgreSQL",
+      "MySQL",
+      "MERN Stack",
+      "MEAN Stack",
+      "Frontend Development",
+      "Backend Development",
+      "Web Development",
+      "Full Stack Development",
+      "React.js",
+      "Vue.js",
+      "Next.js",
+      "Node.js",
+      "Express.js",
+      "AI",
+      "JavaScript",
+      "TypeScript",
+      "HTML & CSS",
+      "Web3",
+      "Solana",
+      "Rust",
+      "C#",
+      "ASP.NET",
+      "Blockchain",
+      "Smart Contracts",
+      "Solidity",
+      "DevOps",
+      "CI/CD",
+      "Git & GitHub",
+      "APIs",
+      "REST",
+      "GraphQL",
+      "Firebase",
+      "AWS",
+      "GCP",
+      "SaaS",
+      "PaaS",
+      "CMS",
+      "Linux",
+      "Agile",
+      "Scrum",
+      "Testing & QA",
+      "Unit Testing",
+      "UI/UX Design",
+      "Figma",
+      "Tailwind CSS",
+    ];
+
     const prompt = `
     Generate a **random** tech-related tweet (under 280 chars) with **3 hashtags**.  
     **Rules:**
-    - Topics:  MERN STACK.
+    - Topics:  ${topics[Math.floor(Math.random() * topics.length)]}
     _ Topics should like facts or any short knowledge that can be used to explain the topic.
     - Hashtags must match the topic.
   `;
